8df9749b8636b2d677efcf1da29ee1ef1b79eaf1
Nicolas Ramirez <ramirezcgn@gmail.com>
2025-08-21 20:40:34 -0300

    Removed logs

--- tp-devops-fe/src/services/todoService.ts
+++ tp-devops-fe/src/services/todoService.ts
@@ -8,10 +8,6 @@ const API_PORT =
   process.env.BE_PORT || process.env.NEXT_PUBLIC_BE_PORT || '3001';
 const API_URL = `${API_SCHEMA}://${API_HOST}:${API_PORT}/api/todos`;
 
-console.log('API_HOST:', API_HOST);
-console.log('API_PORT:', API_PORT);
-console.log('Using API_URL:', API_URL);
-
 const handleResponse = async (res: Response) => {
   if (!res.ok) {
     let errorMsg = 'Error en la comunicación con el servidor';
